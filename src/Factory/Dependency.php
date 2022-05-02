ddcd5dbcda1f1f429d41f0579c0e87860080878b
Peter Kavanagh <peter@tensorgroup.org>
2022-05-02 20:22:58 +1000

    Added auto deploy wp-config on composer install

--- src/Factory/Dependency.php
+++ src/Factory/Dependency.php
@@ -6,15 +6,22 @@ class Dependency {
 
   static function install() {
     echo "Grabbing WP-Factory dependencies.";
+    // Factory Utils
     Dependency::getAndPutFile("https://gist.github.com/53cf60dd16491e9c1795b5ca08d5ce3e.git", "public/wp-content/mu-plugins/", "factory-utils.php");
+    // Construct "wp-config.php" for local.
+    exec("/bin/sh ".__DIR__."/build-wp-config.sh");
   }
 
   static function getAndPutFile($fileUrl, $path, $filename) {
-    $file = file_get_contents($fileUrl);
-    mkdir($path, 0777, true);
-    $filePath = fopen($path.$filename, 'w');
-    fwrite($filePath, $file);
-    fclose($filePath);
+    if (!file_exists($path.$filename)) {
+      $file = file_get_contents($fileUrl);
+      if (!mkdir($path, 0777, true)) {
+        echo "couldn't create path";
+      };
+      $filePath = fopen($path.$filename, 'w');
+      fwrite($filePath, $file);
+      fclose($filePath);
+    }
   }
 
 }
